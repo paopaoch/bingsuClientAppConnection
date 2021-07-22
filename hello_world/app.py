1af2e36f17970838d0fa3586d915d857a87ecea6
paopaoch <paopaochofficial@gmail.com>
2021-07-22 17:57:35 +0700

    add google api

--- hello_world/app.py
+++ hello_world/app.py
@@ -1,33 +1,32 @@
 import json
-
-# import requests
+import requests
 import boto3
 
 
 def lambda_handler(event, context):
-    client = boto3.client('location')
+    item = event['arguments']
+    source = item['source']
+    dest = item['dest']
 
-    response = client.calculate_route(
-        CalculatorName='GetDistanceTimeForDelivery',
-        CarModeOptions={
-            'AvoidFerries': True,
-            'AvoidTolls': False
-        },
-        DepartNow=True,
-        DeparturePosition=[
-            13.725520, 100.584480
-        ],
-        DestinationPosition=[
-            13.737418, 100.399955,
-        ],
-        DistanceUnit='Kilometers',
-        TravelMode='Car',
-    )
+    # ------For finding distance, time, speed------
+    api_key ='AIzaSyBiOi79T_MesgMRTSnHqcCmlfLEFpNEjq8' 
+    url ='https://maps.googleapis.com/maps/api/distancematrix/json?'
+    
+    r = requests.get(url + 'origins=' + source +
+                    '&destinations=' + dest +
+                    '&departure_time=now' +
+                    '&key=' + api_key)
+    x = r.json()
+
+    distance_km = int(x['rows'][0]['elements'][0]['distance']['value']) / 1000
+    time_hr = int(x['rows'][0]['elements'][0]['duration']['value']) / 3600
+    avg_speed_kmph = distance_km / time_hr
 
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello world",
-            # "location": ip.text.replace("\n", "")
+            "distance": distance_km,
+            "time" : time_hr,
+            "speed" : avg_speed_kmph
         }),
     }
